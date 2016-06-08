ff76ca13fe779f1547b19d6ad270ee0450999bb3
Alan Denison and Brian Grant <alan@>
2016-06-08 08:25:50 -0700

    add check for invalid entry: whitespace or other characters then numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0) && (year > 0)) {
     return true;
+  } else if (!year) {
+    alert("Enter valid number.");
+
   } else {
     return false;
   }
